6ff46752f9a8f01fe59df53fe0dfd9e519950ae2
Himanshu <himanshu.kumarr07@gmail.com>
2023-09-06 20:40:46 +0530

    Bug: Fixed✨

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose"
 
 export const connectDB = async () => {
-    const { connection } = await mongoose.connect(process.env.MONGO_URI)
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
 
-
-    console.log(`MongoDB connected with ${connection.host}`)
-}
+        console.log(`MongoDB connected with ${mongoose.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+    }
+};
