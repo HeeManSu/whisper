80f67512a69d2df4a3de9c63472d03b69aa44771
Himanshu <himanshu.kumarr07@gmail.com>
2023-09-07 16:14:02 +0530

    Revert "Bug: Fixed✨"
    This reverts commit 6ff46752f9a8f01fe59df53fe0dfd9e519950ae2.

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,8 @@
 import mongoose from "mongoose"
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+    const { connection } = await mongoose.connect(process.env.MONGO_URI)
 
-        console.log(`MongoDB connected with ${mongoose.connection.host}`);
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-    }
-};
+
+    console.log(`MongoDB connected with ${connection.host}`)
+}
